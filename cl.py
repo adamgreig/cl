aa01e48a222a2f83c002ee133214444ad8b57d07
Adam Greig <adam@adamgreig.com>
2020-08-26 22:25:09 +0100

    Add simple RGMII TX

--- cl.py
+++ cl.py
@@ -130,6 +130,76 @@ class RGMIIRx(nm.Elaboratable):
         return m
 
 
+class RGMIITx(nm.Elaboratable):
+    def __init__(self, read_port):
+        # Inputs
+        self.dv = nm.Signal()
+        self.n = nm.Signal(11)
+
+        # Outputs
+        self.txd = nm.Signal(8)
+        self.txctl = nm.Signal(2)
+        self.ready = nm.Signal()
+
+        self.rp = read_port
+
+    def elaborate(self, platform):
+        m = nm.Module()
+        n = nm.Signal(11)
+
+        txen = nm.Signal()
+        txerr = nm.Signal()
+        m.d.comb += txerr.eq(0)
+        m.d.comb += self.txctl[0].eq(txen), self.txctl[1].eq(txen ^ txerr)
+        m.d.comb += self.txd.eq(self.rp.data)
+
+        with m.FSM():
+            with m.State("IDLE"):
+                m.d.comb += self.ready.eq(1)
+                m.d.sync += n.eq(self.n)
+                with m.If(self.dv):
+                    m.d.sync += txen.eq(1), self.rp.addr.eq(1)
+                    m.next = "DATA"
+                with m.Else():
+                    m.d.sync += txen.eq(0), self.rp.addr.eq(0)
+            with m.State("DATA"):
+                m.d.comb += self.ready.eq(0)
+                m.d.sync += txen.eq(1), self.rp.addr.eq(self.rp.addr + 1)
+                with m.If(self.rp.addr == n - 1):
+                    m.next = "IDLE"
+
+        return m
+
+
+def test_rgmii_tx():
+    from nmigen.sim import pysim
+    m = nm.Module()
+    mem = nm.Memory(width=8, depth=64, init=b"Hello World! Ignore this.")
+    m.submodules.rp = rp = mem.read_port(transparent=False)
+    m.submodules.tx = tx = RGMIITx(rp)
+
+    def tb():
+        yield
+        yield
+        yield tx.n.eq(12)
+        yield tx.dv.eq(1)
+        yield
+        yield tx.n.eq(0)
+        yield tx.dv.eq(0)
+        data = []
+        for _ in range(16):
+            yield
+            if (yield tx.txctl[0]):
+                data.append((yield tx.txd))
+        assert bytes(data) == b"Hello World!"
+
+    sim = pysim.Simulator(m)
+    sim.add_clock(1/125e6)
+    sim.add_sync_process(tb)
+    with sim.write_vcd("rgmii_tx.vcd"):
+        sim.run()
+
+
 def test_mem_to_parallel():
     from nmigen.sim import pysim
     m = nm.Module()
@@ -243,7 +313,7 @@ class ColorLite5A75E_V6_0_Platform(LatticeECP5Platform):
     outputs = ['A7', 'A15', 'E12', 'E13', 'K12', 'M6', 'M13']
     bidis = ['D12']
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         lvcmos = self.lvcmos
 
         # Create resources for each LED header
@@ -264,7 +334,7 @@ class ColorLite5A75E_V6_0_Platform(LatticeECP5Platform):
             self.resources += [Resource(pin, 0, Pins(pin, dir="i"), lvcmos)]
         for pin in self.bidis:
             self.resources += [Resource(pin, 0, Pins(pin, dir="io"), lvcmos)]
-        super().__init__()
+        super().__init__(*args, **kwargs)
 
 
 class Top(nm.Elaboratable):
@@ -282,24 +352,26 @@ class Top(nm.Elaboratable):
         m.d.sync += ctr.eq(ctr + 1)
         m.d.comb += led.o.eq(ctr[-1])
 
-        # Hold PHYs in reset
-        """
-        m.d.comb += platform.request("eth_common").rst.eq(1)
-        """
+        # Enable PHYs
+        m.d.comb += platform.request("eth_common").rst.eq(0)
 
-        # Connect to RX of PHY1
-        phy1 = platform.request("phy", 1, xdr={"rxd": 2, "rxctl": 2})
+        # Connect PHY1
+        phy1 = platform.request(
+            "phy", 1, xdr={"rxd": 2, "rxctl": 2, "txd": 2, "txctl": 2})
         m.domains.rxc = cd_rxc = nm.ClockDomain("rxc")
         m.d.comb += [
-            cd_rxc.clk.eq(phy1.rxc),
-            phy1.rxd.i_clk.eq(~phy1.rxc),
-            phy1.rxctl.i_clk.eq(phy1.rxc),
+            cd_rxc.clk.eq(phy1.rxc.i),
+            phy1.rxd.i_clk.eq(phy1.rxc.i),
+            phy1.rxctl.i_clk.eq(phy1.rxc.i),
+            phy1.txc.o.eq(phy1.rxc.i),
+            phy1.txd.o_clk.eq(phy1.rxc.i),
+            phy1.txctl.o_clk.eq(phy1.rxc.i),
         ]
 
         # Create RGMII receiver
         dr_rxc = nm.DomainRenamer("rxc")
         rxmem = nm.Memory(width=8, depth=2048)
-        m.submodules.rx_rp = rx_rp = dr_rxc(rxmem.read_port(transparent=False))
+        m.submodules.rx_rp = rx_rp = rxmem.read_port(transparent=False)
         m.submodules.rx_wp = rx_wp = dr_rxc(rxmem.write_port())
         m.submodules.rgmii_rx = rgmii_rx = dr_rxc(RGMIIRx(rx_wp))
         m.d.comb += [
@@ -307,8 +379,48 @@ class Top(nm.Elaboratable):
             rgmii_rx.rxctl.eq(nm.Cat(phy1.rxctl.i0, phy1.rxctl.i1)),
         ]
 
+        # Dummy TX packet sends some UDP data
+        txpacket = [
+            # Preamble and SFD
+            0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0xD5,
+            # Destination MAC address
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            # Source MAC address is made up locally-administered
+            0x02, 0x00, 0x01, 0x02, 0x03, 0x04,
+            # IPv4 ethertype
+            0x08, 0x00,
+            # IPv4 header
+            0x45, 0x00, 0x04, 0x1c,
+            0x00, 0x00, 0x40, 0x00,
+            0x40, 0x11, 0x22, 0xc1,
+            0x0a, 0x00, 0x00, 0x10,
+            0x0a, 0x00, 0x00, 0x01,
+            # UDP header
+            0xb7, 0x69, 0x04, 0xd2,
+            0x04, 0x08, 0x00, 0x00,
+            # UDP payload
+        ] + list(range(256))*4 + [
+            # FCS
+            0xc1, 0x35, 0x83, 0xf2,
+        ]
+
+        # Create RGMII transmitter
+        txmem = nm.Memory(width=8, depth=2048, init=txpacket)
+        m.submodules.tx_rp = tx_rp = dr_rxc(txmem.read_port(transparent=False))
+        m.submodules.rgmii_tx = rgmii_tx = dr_rxc(RGMIITx(tx_rp))
+        m.d.comb += [
+            nm.Cat(phy1.txd.o0, phy1.txd.o1).eq(rgmii_tx.txd),
+            nm.Cat(phy1.txctl.o0, phy1.txctl.o1).eq(rgmii_tx.txctl),
+        ]
+        ctr = nm.Signal(5)
+        m.d.comb += rgmii_tx.n.eq(len(txpacket)), rgmii_tx.dv.eq(ctr == 16)
+        with m.If(rgmii_tx.ready):
+            m.d.sync += ctr.eq(ctr + 1)
+        with m.Else():
+            m.d.sync += ctr.eq(0)
+
         # Dump received packets over parallel interface
-        m.submodules.m2p = m2p = dr_rxc(Mem2Parallel(rx_rp, div=5))
+        m.submodules.m2p = m2p = Mem2Parallel(rx_rp, div=2)
         m.d.comb += m2p.n.eq(rgmii_rx.n), m2p.dv.eq(rgmii_rx.dv)
         led_j7 = platform.request("led_rgb", 6)
         led_j8 = platform.request("led_rgb", 7)
@@ -334,7 +446,7 @@ class Top(nm.Elaboratable):
 
 
 def main():
-    platform = ColorLite5A75E_V6_0_Platform()
+    platform = ColorLite5A75E_V6_0_Platform(toolchain="Trellis")
     platform.build(Top(), ecppack_opts=["--compress"])
     subprocess.run(["ffp", "ecp5", "program", "build/top.bit"])
 
